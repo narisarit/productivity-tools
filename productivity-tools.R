99fbb46ec442897ad62f1cbe580aeba25146038e
narisarit <rast777@naver.com>
2025-01-23 17:24:47 +0900

    codes for productivity tools

---
+++ productivity-tools.R
@@ -0,0 +1,64 @@
+# codes below are for unix-like environment(gitbash, r terminal)
+echo
+pwd
+~
+/
+ls # -a, -l, -t, -r, -lart
+cd
+mkdir
+rmdir
+rm # -r, -f, -rf
+..
+.
+-
+mv
+less
+q
+--help
+| #pipe
+  
+*
+?
+  
+echo $HOME
+echo $PATH
+env
+echo $SHELL
+
+start # starts file
+nano # a bare-bones text editor
+ln # create a symbolic link
+tar # archive files and subdirectoreis of a directory into one file
+ssh #connect to another computer
+grep #search for patterns in a file
+awk/sed #two powerful commands that premit you to find specific strings in files and change them
+
+
+
+
+
+git clone
+git status
+git commit
+git add
+git log
+git push
+git fetch
+git merge
+git init
+git remote add origin
+
+
+
+# codes below are for R studio console
+getwd()
+save()
+ggsave()
+
+
+# R markdown
+library(knitr)
+output: html_document 
+output: pdf_document 
+output: word_document
+output: github_document
